3527dfdc4c91597527cf320bc290eecc93520616
piotrcurious <78758456+piotrcurious@users.noreply.github.com>
2025-01-08 14:29:07 +0100

    Add files via upload

---
+++ exp_growth_junkbox/color/color_multi/AdvancedTFTVisualizer.cpp
@@ -0,0 +1,55 @@
+#include "AdvancedTFTVisualizer.hpp"
+#include "config.h"
+
+AdvancedTFTVisualizer::AdvancedTFTVisualizer() : display(TFT_eSPI()) {}
+
+//private :
+float AdvancedTFTVisualizer::mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+        return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+//public :
+
+void AdvancedTFTVisualizer::begin() {
+        display.init();
+        display.setRotation(1);  // Landscape mode
+        display.fillScreen(TFT_BLACK);
+    }
+
+void AdvancedTFTVisualizer::visualizeGrowthAnalysis(const std::vector<float>& xData,
+                                 const std::vector<float>& yData,
+                                 const std::vector<float>& coeffs,
+                                 bool growthDetected,
+                                 float growthRate) {
+        display.fillScreen(TFT_BLACK);
+
+        float xMin = *std::min_element(xData.begin(), xData.end());
+        float xMax = *std::max_element(xData.begin(), xData.end());
+        float yMin = *std::min_element(yData.begin(), yData.end());
+        float yMax = *std::max_element(yData.begin(), yData.end());
+
+        for (size_t i = 0; i < xData.size(); ++i) {
+            int x = mapFloat(xData[i], xMin, xMax, 0, TFT_SCREEN_WIDTH - 1);
+            int y = mapFloat(yData[i], yMin, yMax, TFT_SCREEN_HEIGHT - 40, 0);
+            display.drawPixel(x, y, TFT_WHITE);
+        }
+
+        for (int x = 0; x < TFT_SCREEN_WIDTH; ++x) {
+            float dataX = mapFloat(x, 0, TFT_SCREEN_WIDTH - 1, xMin, xMax);
+
+            float yFitted = 0.0;
+            for (size_t j = 0; j < coeffs.size(); ++j) {
+                yFitted += coeffs[j] * pow(dataX - xMin, j);
+            }
+
+            int y = mapFloat(yFitted, yMin, yMax, TFT_SCREEN_HEIGHT - 40, 0);
+            display.drawPixel(x, y, TFT_GREEN);
+        }
+
+        display.setCursor(10, TFT_SCREEN_HEIGHT - 30);
+        display.setTextColor(TFT_WHITE);
+        display.printf("Growth: %s", growthDetected ? "YES" : "NO");
+        display.setCursor(10, TFT_SCREEN_HEIGHT - 20);
+        display.printf("Rate: %.2f", growthRate);
+        //display.display();
+    }
---
+++ exp_growth_junkbox/color/color_multi/color_multi.ino
@@ -0,0 +1,64 @@
+#include "config.h"
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include "ExponentialGrowthDetector.hpp"
+
+#ifdef USE_OLED
+#include "AdvancedOLEDVisualizer.hpp"
+AdvancedOLEDVisualizer oledViz;
+#endif //#ifdef USE_OLED
+
+#ifdef USE_ESPI
+#include "AdvancedTFTVisualizer.hpp"
+AdvancedTFTVisualizer tftViz;
+#endif //#ifdef USE_ESPI
+
+ExponentialGrowthDetector growthDetector;
+
+void setup() {
+    Serial.begin(115200);
+     // Initialize growth detector and OLED
+    growthDetector.begin();
+
+#ifdef USE_OLED
+    oledViz.begin();
+#endif //#ifdef USE_OLED
+
+#ifdef USE_ESPI
+    tftViz.begin();
+#endif //#ifdef USE_ESPI
+
+      
+}
+float exp_step = 0 ; 
+float value = 0 ;
+void loop() {
+    // Simulate exponential-like data collection
+    static float time = 0;
+    
+    // Simulated exponential growth with noise
+   // float value = 1 * sin(0.2*time)*exp(0.1 *time)) + random(-80, 80) / 10.0;
+
+    float time_delta = random(0,+1000)/1000.0;
+    float ride = 1*sin(0.2*time) ;
+
+    if (ride >0) {exp_step += time_delta; 
+    } else { exp_step = 0 ;}
+    
+    value = 60* sin(0.1*time)+10*exp(exp_step*0.2)+ random(-80, 80) / 100.0;
+    time += time_delta;
+    growthDetector.addDataPoint(time, value);
+    // Detect growth and visualize
+    growthDetector.detectExponentialGrowth();
+
+#ifdef USE_OLED
+    oledViz.visualizeGrowthAnalysis(growthDetector.timestamps, growthDetector.values, growthDetector.bestCoeffs, growthDetector.growthDetected, growthDetector.bestGrowthRate);
+#endif //#ifdef USE_OLED
+
+#ifdef USE_ESPI
+    tftViz.visualizeGrowthAnalysis(growthDetector.timestamps, growthDetector.values, growthDetector.bestCoeffs, growthDetector.growthDetected, growthDetector.bestGrowthRate);
+#endif //#ifdef USE_ESPI
+    
+//    delay(200);
+}
